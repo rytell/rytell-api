32ae20f3969b8f8b483e6673a7ba2ea0e27cf757
bulchack <jesuseovalles@gmail.com>
2022-02-10 21:07:26 -0300

    Endpoint for annual volume estimate

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { request } from 'graphql-request';
+import { getDailyVolume, graphUrl } from './rytellswap/graphql/queries';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Welcome to Rytell API';
+  }
+
+  async getAnnualProjection(): Promise<any> {
+    const { rytellDayDatas } = await request(graphUrl, getDailyVolume)
+    let totalVolume = 0
+    rytellDayDatas.map( day => {
+      totalVolume += parseFloat(day.dailyVolumeUSD)
+    })
+
+    const dailyAvg = totalVolume / rytellDayDatas.length;
+    const totalAnnualEstimate = dailyAvg * 365
+    const annualProjection = totalAnnualEstimate * 0.03
+
+    const response = {
+      volumeLast30Days: totalVolume,
+      dailyAvg,
+      totalAnnualEstimate,
+      annualProjection
+    }
+    return response
   }
 }
